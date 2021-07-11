cd0d77c23e8d082b465755e49f5819b8b6e630b0
Yiteng Huang <hyy528924543@gmail.com>
2021-07-12 09:58:04 +1000

    change config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,44 @@
+const configs = {
+  distDir: "dest",
+
+  generateEtags: true,
+
+  onDemandEntries: {
+    maxInactiveAge: 25 * 1000,
+    pagesBufferLength: 2,
+  },
+
+  pageExtensions: ["jsx", "js"],
+
+  generateBuildId: async () => {
+    if (process.env.YOUR_BUILD_ID) {
+      return process.env.YOUR_BUILD_ID;
+    }
+    return null;
+  },
+
+  webpack(config, options) {
+    return config;
+  },
+
+  webpackDevMiddleware: (config) => {
+    return config;
+  },
+
+  env: {
+    customeKey: "value",
+  },
+
+  serverRuntimeConfig: {
+    mySecret: "secret",
+    secondSecret: process.env.SECOND_SECRET,
+  },
+
+  publicRuntimeConfig: {
+    staticFolder: "/static",
+  },
+};
+
 module.exports = {
   reactStrictMode: true,
-}
+};
