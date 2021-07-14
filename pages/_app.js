829c5c9943422364b1cd7f8ddcfd38eddf553446
Yiteng Huang <hyy528924543@gmail.com>
2021-07-14 11:30:15 +1000

    add loading animation

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,58 @@
+import { useState, useEffect } from "react";
 import App from "next/app";
 import { Provider } from "react-redux";
-import testHoc from "../lib/with-redux";
+import Hoc from "../lib/with-redux";
 import Layout from "../components/Layout";
+import PageLoading from "../components/PageLoading";
 import "../styles/globals.css";
 import "antd/dist/antd.css";
+import Link from "next/link";
+import router from "next/router";
 
 function MyApp({ Component, pageProps, reduxStore }) {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const changeIntoLoading = () => {
+    setIsLoading(true);
+  };
+  const changeNotLoading = () => {
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    router.events.on("routeChangeStart", changeIntoLoading);
+    router.events.on("routeChangeComplete", changeNotLoading);
+    router.events.on("routeChangeError", changeNotLoading);
+    return () => {
+      router.events.off("routeChangeStart", changeIntoLoading);
+      router.events.off("routeChangeComplete", changeNotLoading);
+      router.events.off("routeChangeError", changeNotLoading);
+    };
+  }, [router.events]);
+
+  MyApp.getInitialProps = async (ctx) => {
+    let appProps;
+    if (App.getInitialProps) {
+      const appProps = await App.getInitialProps(ctx);
+      return { ...appProps };
+    }
+    return appProps;
+  };
+
   return (
     <Provider store={reduxStore}>
+      {isLoading ? <PageLoading /> : null}
       <Layout>
+        <Link href="/">
+          <a>Index</a>
+        </Link>
+        <Link href="/detail">
+          <a>Detail</a>
+        </Link>
         <Component {...pageProps} />
       </Layout>
     </Provider>
   );
 }
 
-MyApp.getInitialProps = async (ctx) => {
-  let appProps;
-  if (App.getInitialProps) {
-    const appProps = await App.getInitialProps(ctx);
-    return { ...appProps };
-  }
-  return appProps;
-};
-
-export default testHoc(MyApp);
+export default Hoc(MyApp);
