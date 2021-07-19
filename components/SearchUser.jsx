84fd6acaac1015c66f6f39dc775cd933d2a34717
Yiteng Huang <hyy528924543@gmail.com>
2021-07-19 11:23:31 +1000

    add a search tag to search creator of issues

---
+++ components/SearchUser.jsx
@@ -0,0 +1,74 @@
+import { useState, useCallback, useRef } from "react";
+import { Select, Spin } from "antd";
+import debounce from "lodash/debounce";
+
+import api from "../lib/api";
+
+const Option = Select.Option;
+
+function SearchUser({ onChange, value }) {
+  const lastFetchIdRef = useRef(0);
+  const [fetching, setFetching] = useState(false);
+  const [options, setOptions] = useState([]);
+
+  const fetchUser = useCallback(
+    debounce((value) => {
+      //   console.log("fetching user", value);
+
+      lastFetchIdRef.current += 1;
+      const fetchId = lastFetchIdRef.current;
+      setFetching(true);
+      setOptions([]);
+
+      api
+        .request({
+          url: `/search/users?q=${value}`,
+        })
+        .then((resp) => {
+          //   console.log("user: ", resp);
+          if (fetchId !== lastFetchIdRef.current) return;
+
+          const data = resp.data.items.map((user) => ({
+            text: user.login,
+            value: user.login,
+          }));
+
+          setFetching(false);
+          setOptions(data);
+        })
+        .catch((err) => {
+          setFetching(false);
+          setOptions([]);
+        });
+    }, 500),
+    []
+  );
+
+  const handleChange = (value) => {
+    setFetching(false);
+    setOptions([]);
+    onChange(value);
+  };
+
+  return (
+    <Select
+      showSearch={true}
+      notFoundContent={fetching ? <Spin size="small" /> : <span>...</span>}
+      filterOption={false}
+      placeholder="creator"
+      value={value}
+      onSearch={fetchUser}
+      onChange={handleChange}
+      allowClear={true}
+      style={{ width: 200 }}
+    >
+      {options.map((option) => (
+        <Option value={option.value} key={option.value}>
+          {option.text}
+        </Option>
+      ))}
+    </Select>
+  );
+}
+
+export default SearchUser;
--- pages/detail/issues.js
+++ pages/detail/issues.js
@@ -3,6 +3,7 @@ import { Avatar, Button } from "antd";
 import dynamic from "next/dynamic";
 
 import withRepoBasic from "../../components/with-repo-basic";
+import SearchUser from "../../components/SearchUser";
 
 import api from "../../lib/api";
 import { getLastUpdated } from "../../lib/util";
@@ -99,8 +100,15 @@ function IssueItem({ issue }) {
 
 const Issues = ({ issues }) => {
   // console.log(issues);
+  const [creator, setCreator] = useState("");
+
+  const handleCreatorChange = useCallback((value) => {
+    setCreator(value);
+  }, []);
+
   return (
     <div className="root">
+      <SearchUser onChange={handleCreatorChange} value={creator} />
       <div className="issues">
         {issues.map((issue) => (
           <IssueItem issue={issue} key={issue.id} />
